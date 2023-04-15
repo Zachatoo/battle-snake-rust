173ccbea4d6a8aafb5792cd0d362042e002757ce
Zachatoo <Zachatoo@users.noreply.github.com>
2023-04-14 20:37:51 -0600

    Add test for semi-safe moves #4

--- src/tests.rs
+++ src/tests.rs
@@ -213,3 +213,90 @@ fn movement_avoid_snake_bodies() {
         .expect("failed to parse response");
     assert_eq!(parsed_body.chosen_move, "left");
 }
+
+#[test]
+fn movement_prefer_safe_move_to_semisafe_move() {
+    let client = Client::untracked(rocket()).expect("Failed to create client instance");
+    let response = client
+        .post("/move?x-api-key=valid_api_key")
+        .header(ContentType::JSON)
+        .body(
+            r#"{
+                "game": {
+                  "id": "unique-game-id",
+                  "ruleset": {
+                    "name": "standard"
+                  },
+                  "timeout": 500
+                },
+                "turn": 0,
+                "board": {
+                  "height": 11,
+                  "width": 11,
+                  "food": [],
+                  "hazards": [],
+                  "snakes": [
+                    {
+                      "id": "my-snake",
+                      "name": "My Snake",
+                      "health": 54,
+                      "body": [
+                        {"x": 6, "y": 5},
+                        {"x": 7, "y": 5},
+                        {"x": 8, "y": 5}
+                      ],
+                      "latency": "111",
+                      "head": {"x": 6, "y": 5},
+                      "length": 3
+                    },
+                    {
+                      "id": "snake-1",
+                      "name": "Snake 1",
+                      "health": 54,
+                      "body": [
+                        {"x": 4, "y": 5},
+                        {"x": 3, "y": 5},
+                        {"x": 2, "y": 5},
+                        {"x": 1, "y": 5}
+                      ],
+                      "latency": "111",
+                      "head": {"x": 4, "y": 5},
+                      "length": 4
+                    },
+                    {
+                      "id": "snake-2",
+                      "name": "Snake 2",
+                      "health": 54,
+                      "body": [
+                        {"x": 6, "y": 4},
+                        {"x": 7, "y": 4},
+                        {"x": 8, "y": 4}
+                      ],
+                      "latency": "111",
+                      "head": {"x": 6, "y": 4},
+                      "length": 3
+                    }
+                  ]
+                },
+                "you": {
+                  "id": "my-snake",
+                  "name": "My Snake",
+                  "health": 54,
+                  "body": [
+                    {"x": 6, "y": 5},
+                    {"x": 7, "y": 5},
+                    {"x": 8, "y": 5}
+                  ],
+                  "latency": "111",
+                  "head": {"x": 6, "y": 5},
+                  "length": 3
+                }
+              }"#,
+        )
+        .dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let parsed_body = response
+        .into_json::<MoveResponse>()
+        .expect("failed to parse response");
+    assert_eq!(parsed_body.chosen_move, "up");
+}
