05a64befb202bd691354d1b831924e226215a8cd
Zachatoo <Zachatoo@users.noreply.github.com>
2023-04-16 23:40:29 -0600

    Add test to ensure food scanning can wrap around snakes Skipping test for now until I can get it to pass.

--- src/tests.rs
+++ src/tests.rs
@@ -515,3 +515,94 @@ fn movement_prefer_food_avoid_self() {
     assert!(parsed_body.shout.contains("up"));
     assert!(parsed_body.shout.contains("down"));
 }
+
+// TODO: Get this test to pass
+#[ignore]
+#[test]
+fn movement_prefer_food_long_scan() {
+    let client = Client::untracked(rocket()).expect("Failed to create client instance");
+    let response = client
+        .post("/move?x-api-key=valid_api_key")
+        .header(ContentType::JSON)
+        .body(
+            r#"{
+              "game": {
+                "id": "unique-game-id",
+                "ruleset": {
+                  "name": "standard"
+                },
+                "timeout": 500
+              },
+              "turn": 0,
+              "board": {
+                "height": 11,
+                "width": 11,
+                "food": [
+                  { "x": 2, "y": 10 }
+                ],
+                "hazards": [],
+                "snakes": [
+                  {
+                    "id": "my-snake",
+                    "name": "My Snake",
+                    "health": 54,
+                    "body": [
+                      {"x": 6, "y": 0},
+                      {"x": 6, "y": 1},
+                      {"x": 7, "y": 1},
+                      {"x": 8, "y": 1},
+                      {"x": 9, "y": 1},
+                      {"x": 9, "y": 2},
+                      {"x": 9, "y": 3}
+                    ],
+                    "latency": "111",
+                    "head": {"x": 6, "y": 0},
+                    "length": 7
+                  },
+                  {
+                    "id": "snake-1",
+                    "name": "Snake 1",
+                    "health": 54,
+                    "body": [
+                      {"x": 3, "y": 0},
+                      {"x": 4, "y": 0},
+                      {"x": 4, "y": 1},
+                      {"x": 5, "y": 1},
+                      {"x": 5, "y": 2},
+                      {"x": 5, "y": 3},
+                      {"x": 5, "y": 4},
+                      {"x": 5, "y": 5}
+                    ],
+                    "latency": "111",
+                    "head": {"x": 3, "y": 0},
+                    "length": 8
+                  }
+                ]
+              },
+              "you": {
+                "id": "my-snake",
+                "name": "My Snake",
+                "health": 54,
+                "body": [
+                  {"x": 6, "y": 0},
+                  {"x": 6, "y": 1},
+                  {"x": 7, "y": 1},
+                  {"x": 8, "y": 1},
+                  {"x": 9, "y": 1},
+                  {"x": 9, "y": 2},
+                  {"x": 9, "y": 3}
+                ],
+                "latency": "111",
+                "head": {"x": 6, "y": 0},
+                "length": 7
+              }
+            }"#,
+        )
+        .dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    let parsed_body = response
+        .into_json::<MoveResponse>()
+        .expect("failed to parse response");
+    assert_eq!(parsed_body.chosen_move, "right");
+    assert_eq!(parsed_body.shout, "right");
+}
