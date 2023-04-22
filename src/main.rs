93db9949ad1c7c34459111f533d07c35eb12910f
Zachatoo <Zachatoo@users.noreply.github.com>
2023-04-22 01:45:22 -0600

    Rename route methods

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,12 @@ mod response;
 mod snakes;
 
 #[get("/")]
-fn rusty_handle_index(_key: auth::ApiKey<'_>) -> Json<Value> {
+fn handle_index_rusty(_key: auth::ApiKey<'_>) -> Json<Value> {
     Json(json!(snakes::rusty::info()))
 }
 
 #[post("/start", format = "json", data = "<start_req>")]
-fn rusty_handle_start(start_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Status {
+fn handle_start_rusty(start_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Status {
     snakes::rusty::start(
         &start_req.game,
         &start_req.turn,
@@ -37,7 +37,7 @@ fn rusty_handle_start(start_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Sta
 }
 
 #[post("/move", format = "json", data = "<move_req>")]
-fn rusty_handle_move(move_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Json<Value> {
+fn handle_move_rusty(move_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Json<Value> {
     let response = snakes::rusty::get_move(
         &move_req.game,
         &move_req.turn,
@@ -49,19 +49,19 @@ fn rusty_handle_move(move_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Json<
 }
 
 #[post("/end", format = "json", data = "<end_req>")]
-fn rusty_handle_end(end_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Status {
+fn handle_end_rusty(end_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Status {
     snakes::rusty::end(&end_req.game, &end_req.turn, &end_req.board, &end_req.you);
 
     Status::Ok
 }
 
 #[get("/")]
-fn righty_handle_index(_key: auth::ApiKey<'_>) -> Json<Value> {
+fn handle_index_righty(_key: auth::ApiKey<'_>) -> Json<Value> {
     Json(json!(snakes::righty::info()))
 }
 
 #[post("/start", format = "json", data = "<start_req>")]
-fn righty_handle_start(start_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Status {
+fn handle_start_righty(start_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Status {
     snakes::righty::start(
         &start_req.game,
         &start_req.turn,
@@ -73,7 +73,7 @@ fn righty_handle_start(start_req: Json<GameState>, _key: auth::ApiKey<'_>) -> St
 }
 
 #[post("/move", format = "json", data = "<move_req>")]
-fn righty_handle_move(move_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Json<Value> {
+fn handle_move_righty(move_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Json<Value> {
     let response = snakes::righty::get_move(
         &move_req.game,
         &move_req.turn,
@@ -85,7 +85,7 @@ fn righty_handle_move(move_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Json
 }
 
 #[post("/end", format = "json", data = "<end_req>")]
-fn righty_handle_end(end_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Status {
+fn handle_end_righty(end_req: Json<GameState>, _key: auth::ApiKey<'_>) -> Status {
     snakes::righty::end(&end_req.game, &end_req.turn, &end_req.board, &end_req.you);
 
     Status::Ok
@@ -122,28 +122,28 @@ fn rocket() -> _ {
         .mount(
             "/",
             routes![
-                rusty_handle_index,
-                rusty_handle_start,
-                rusty_handle_move,
-                rusty_handle_end
+                handle_index_rusty,
+                handle_start_rusty,
+                handle_move_rusty,
+                handle_end_rusty
             ],
         )
         .mount(
             "/rusty",
             routes![
-                rusty_handle_index,
-                rusty_handle_start,
-                rusty_handle_move,
-                rusty_handle_end
+                handle_index_rusty,
+                handle_start_rusty,
+                handle_move_rusty,
+                handle_end_rusty
             ],
         )
         .mount(
             "/righty",
             routes![
-                righty_handle_index,
-                righty_handle_start,
-                righty_handle_move,
-                righty_handle_end
+                handle_index_righty,
+                handle_start_righty,
+                handle_move_righty,
+                handle_end_righty
             ],
         )
 }
